d258a68f64ae5417b5eb66556b3d272eb78ea89e
markhitchk <mark.hitchk@gmail.com>
2026-07-30 05:41:19 -0700

    Remove remaining repository contents

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'critter-extraction-v0.26.7';
-const CORE = ['./','./index.html','./styles.css','./js/game.js','./manifest.json','./assets/icon.svg','./assets/models/harleys-model-library.js'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
-self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); return response; }).catch(() => caches.match('./index.html')))); });
